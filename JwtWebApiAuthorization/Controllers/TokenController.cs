1b802531026f33c6c4b85211c7fc0eb94fced9aa
Nícolas Fernandes <nicolaspfernandes@outlook.com>
2017-03-06 10:10:23 -0300

    Initial commit.

---
+++ JwtWebApiAuthorization/Controllers/TokenController.cs
@@ -0,0 +1,18 @@
+﻿namespace JwtWebApiAuthorization.Controllers {
+
+    using Token;
+    using Models;
+
+    using System.Web.Http;
+
+    [RoutePrefix("tokens")]
+    public class TokenController : ApiController {
+        
+        [HttpGet, Route("{role:int}")]
+        public CustomResponse Post(Roles role) {
+
+            var authorizationTokenWithRole = new AccessToken { Role = role }.ToTokenString();
+            return new CustomResponse(authorizationTokenWithRole);
+        }
+    }
+}
